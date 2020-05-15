d3a7ff26a8472b712ad00b4ad9ebb4300c936d44
Phil de Joux <philderbeast@gmail.com>
2020-05-15 11:03:23 -0400

    Add a property test for calcDistance, #16.

--- test-suite-units/Main.hs
+++ test-suite-units/Main.hs
@@ -1,8 +1,17 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Main where
 
+import Test.Tasty.TH
+import Test.Tasty.QuickCheck
 import Text.Printf (printf)
-import UoM (Unit(..), calcDistance)
+import UoM (Unit(..), extract, calcDistance)
 
 main :: IO ()
-main =
+main = do
+  putStrLn "UoM examples"
   putStrLn $ printf "3 m/s for 2 s = %s m" (show $ calcDistance (MkUnit 3) (MkUnit 2))
+  putStrLn ""
+  $(defaultMainGenerator)
+
+prop_distance :: Double -> Double -> Bool
+prop_distance x y = x * y == (extract $ calcDistance (MkUnit x) (MkUnit y))
